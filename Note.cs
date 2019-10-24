c1956500ea4ae6680b83f77256bcc76b5b67039c
whatevershut <56258942+whatevershut@users.noreply.github.com>
2019-10-24 20:44:43 +0100

    Add files via upload

---
+++ Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteProject.Models
+{
+    public class Note
+    {
+        int NoteID { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+
+        [ForeignKey("UserID")]
+        public User User { get; set; }
+        public int UserID{ get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteProject.Models
+{
+    public class User
+    {
+        int UserID { get; set; }
+        string Name { get; set; }
+        string LastName { get; set; }
+        string Login { get; set; }
+        string Password { get; set; }
+        Boolean ADM { get; set;  }
+
+
+    }
+}
